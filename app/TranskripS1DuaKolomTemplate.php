1a04dbad97a136b09dc4af831bbf09df7f213b69
Dede Gunawan <dede@unsil.ac.id>
2020-11-25 14:12:55 +0700

    Transkrip Multi line

--- app/TranskripS1DuaKolomTemplate.php
+++ app/TranskripS1DuaKolomTemplate.php
@@ -141,6 +141,26 @@ class TranskripS1DuaKolomTemplate extends BaseTemplate
 
     }
 
+    public function getJumlahBarisIdeal()
+    {
+        $transkrip_akademik = TranskripAkademikUnsil::getInstance();
+        $krs_collection = $transkrip_akademik->getKrsCollection();
+
+        $KRSIDs = [];
+
+        $jumlah = $krs_collection->reduce(function ($acc, $krs) use (&$KRSIDs) {
+            $baris = ceil(strlen($krs['nama_matakuliah']) / 42);
+            if ($baris > 1) $KRSIDs[$krs['id_matakuliah']] = $baris;
+            return ($acc + $baris);
+        }, 0);
+        $barisTotal = ceil($jumlah/2);
+        if ($barisTotal < 40) $barisTotal = 47;
+
+        return [
+            $jumlah, $barisTotal, $KRSIDs
+        ];
+    }
+
     public function BuatIsiTranskrip()
     {
         $transkrip_akademik = TranskripAkademikUnsil::getInstance();
@@ -159,41 +179,81 @@ class TranskripS1DuaKolomTemplate extends BaseTemplate
         }
 
         $ti = $this->getTinggiBaris();
-        $jumlah = $krs_collection->count();
-        $barisTotal = ceil($jumlah/2);
-        if ($barisTotal < 40) $barisTotal = 47;
+//        $jumlah = $krs_collection->count();
+//        $barisTotal = ceil($jumlah/2);
+//        if ($barisTotal < 40) $barisTotal = 47;
+
+        list($jumlah, $barisTotal, $KRSIDs) = $this->getJumlahBarisIdeal();
+
 
         $lastYPosition = $p->GetY();
 
-        foreach ($krs_collection as $n => $krs) {
-            if ($n >= $barisTotal) break;
-            $p->setX(6);
-            $p->Cell(4, $ti, $n+1, 'L', 0, 'C');
-            $p->Cell(19, $ti, $krs['kode_matakuliah'], 'L', 0, 'C');
-            $p->Cell(54, $ti, $krs['nama_matakuliah'], 'L', 0);
-            $p->Cell(5.5, $ti, $krs['sks'], 'L', 0, 'C');
-            $p->Cell(6.5, $ti, $krs['huruf_mutu'], 'L', 0, 'C');
-            $p->Cell(6, $ti, number_format($krs['angka_mutu'], 2, ".", ","), 'L', 0, 'C');
-            $p->Cell(5.5, $ti, $krs->getJumlahBobot(), 'LR', 0, 'C');
+        $realN = 0;
 
-            $p->Ln($ti);
+        foreach ($krs_collection as $n => $krs) {
+
+            if ($realN >= $barisTotal) break;
+
+            $realN++;
+
+            $kali = in_array($krs['id_matakuliah'], array_keys($KRSIDs)) ? $KRSIDs[$krs['id_matakuliah']] : 1;
+
+            $p->setX(6);
+            $p->Cell(4, $ti*$kali, $n+1, 'L', 0, 'C');
+            $p->Cell(19, $ti*$kali, $krs['kode_matakuliah'], 'L', 0, 'C');
+            if ($kali > 1) {
+                $realN = $realN+$kali-1;
+                $lastX = $p->GetX();
+                $lastY = $p->GetY();
+                $p->MultiCell(54, $ti, $krs['nama_matakuliah'], 'L', 'L');
+                $p->SetXY($lastX+54, $lastY);
+            } else {
+                $p->Cell(54, $ti*$kali, $krs['nama_matakuliah'], 'L', 0);
+            }
+
+            $p->Cell(5.5, $ti*$kali, $krs['sks'], 'L', 0, 'C');
+            $p->Cell(6.5, $ti*$kali, $krs['huruf_mutu'], 'L', 0, 'C');
+            $p->Cell(6, $ti*$kali, number_format($krs['angka_mutu'], 2, ".", ","), 'L', 0, 'C');
+            $p->Cell(5.5, $ti*$kali, $krs->getJumlahBobot(), 'LR', 0, 'C');
+
+            $p->Ln($ti*$kali);
         }
 
         $p->SetY($lastYPosition);
 
+        $barisTotal = $n;
+
+        $realN = 0;
+
 
         foreach ($krs_collection as $n => $krs) {
-            if ($n < $barisTotal) continue;
-            $p->setX(108.5);
-            $p->Cell(4, $ti, $n+1, 'L', 0, 'C');
-            $p->Cell(19, $ti, $krs['kode_matakuliah'], 'L', 0, 'C');
-            $p->Cell(54, $ti, $krs['nama_matakuliah'], 'L', 0);
-            $p->Cell(5.5, $ti, $krs['sks'], 'L', 0, 'C');
-            $p->Cell(6.5, $ti, $krs['huruf_mutu'], 'L', 0, 'C');
-            $p->Cell(6, $ti, number_format($krs['angka_mutu'], 2, ".", ","), 'L', 0, 'C');
-            $p->Cell(5.5, $ti, $krs->getJumlahBobot(), 'LR', 0, 'C');
+            $realN++;
 
-            $p->Ln($ti);
+            if ($realN <= $barisTotal) continue;
+
+
+            $kali = in_array($krs['id_matakuliah'], array_keys($KRSIDs)) ? $KRSIDs[$krs['id_matakuliah']] : 1;
+
+            $p->setX(108.5);
+            $p->Cell(4, $ti*$kali, $n+1, 'L', 0, 'C');
+            $p->Cell(19, $ti*$kali, $krs['kode_matakuliah'], 'L', 0, 'C');
+
+            if ($kali > 1) {
+                $realN = $realN+$kali-1;
+                $lastX = $p->GetX();
+                $lastY = $p->GetY();
+                $p->MultiCell(54, $ti, $krs['nama_matakuliah'], 'L', 'L');
+                $p->SetXY($lastX+54, $lastY);
+            } else {
+                $p->Cell(54, $ti*$kali, $krs['nama_matakuliah'], 'L', 0);
+            }
+
+            $p->Cell(5.5, $ti*$kali, $krs['sks'], 'L', 0, 'C');
+            $p->Cell(6.5, $ti*$kali, $krs['huruf_mutu'], 'L', 0, 'C');
+            $p->Cell(6, $ti*$kali, number_format($krs['angka_mutu'], 2, ".", ","), 'L', 0, 'C');
+            $p->Cell(5.5, $ti*$kali, $krs->getJumlahBobot(), 'LR', 0, 'C');
+
+            $p->Ln($ti*$kali);
         }
 
         $this->BuatFooterIsiTranskrip();
